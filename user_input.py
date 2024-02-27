1c642d495080c959b22648bbb76322fccbc2dd2b
pato <patiwagura@gmail.com>
2024-02-27 21:54:00 +0300

    Add user_input.py intro to python assingment

---
+++ user_input.py
@@ -0,0 +1,9 @@
+# Assignment 1: Intro To Python Assignment
+
+# Write a program that asks users : name, age, location and prints a personalized_message.
+name = input("Enter your name : ")
+age = input("Enter your age : ")
+location = input("Enter your location: ")
+
+# print a personalized message.
+print("Hello " + name + ", you are " + age + " years old and live in " + location)
